609043689a53ee8c37936d0017abf489e75239cb
Shayan Zameer <alizadishah2@gmail.com>
2024-07-12 13:16:46 +0100

    changes add

--- src/Pages/Payment/Paymentindex.jsx
+++ src/Pages/Payment/Paymentindex.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Paymentindex = () => {
   return (
-    <div>Paymentindex</div>
-  )
-}
+    <div className="bg-black h-screen p-4">
+      <div className="font-Norwester text-white font-medium text-2xl">
+        <p>Introduction and Instructions</p>
+      </div>
 
-export default Paymentindex
+      <div className="font-leagueSpartan text-base text-white font-normal py-4">
+        <p>
+          Welcome to the Payment Options section. We use GoCardless for secure
+          and efficient direct debit payments. Please provide your bank details
+          below to set up your payment method.
+        </p>
+      </div>
+
+      <div className="font-leagueSpartan text-secandary font-medium text-base">
+        <p>[Learn more about GoCardless]</p>
+      </div>
+
+      <div className="font-Norwester text-white font-medium text-xl py-4">
+        <p>Bank Account Information</p>
+      </div>
+    </div>
+  );
+};
+
+export default Paymentindex;
