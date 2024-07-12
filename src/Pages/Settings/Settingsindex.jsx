62fa359e4b48a28f2d5e36d380c587a832258233
Shayan Zameer <alizadishah2@gmail.com>
2024-07-12 18:51:03 +0100

    changes pushed

--- src/Pages/Settings/Settingsindex.jsx
+++ src/Pages/Settings/Settingsindex.jsx
@@ -1,7 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Editprofile from "./components/Editprofile";
+import Security from "./components/Security";
 const Settingsindex = () => {
-  return <div className="text-7xl">Settingsindex</div>;
+  const [showEditProfile, setShowProfile] = useState(true);
+  const [showSecurity, setShowSecurity] = useState(false);
+  const [showBorder, setShowBorder] = useState("EditProfile");
+
+  const handleEditProfile = () => {
+    setShowProfile(true);
+    setShowSecurity(false);
+    setShowBorder("EditProfile");
+  };
+  const handleSecurity = () => {
+    setShowSecurity(true);
+    setShowProfile(false);
+    setShowBorder("Security");
+  };
+  return (
+    <div className=" bg-black text-white h-screen p-4">
+      <div className="text-white font-Norwester text-2xl border-b border-tertiary  font-normal flex space-x-8">
+        <div
+          onClick={handleEditProfile}
+          className={`p-4 cursor-pointer ${
+            showBorder === "EditProfile" ? "border-b-4 border-secandary" : ""
+          }`}
+        >
+          Edit Profile
+        </div>
+        <div
+          onClick={handleSecurity}
+          className={` ${
+            showBorder === "Security" ? "border-b-4 border-secandary" : ""
+          } p-4 cursor-pointer `}
+        >
+          Security
+        </div>
+      </div>
+
+      {showEditProfile && (
+        <div>
+          <Editprofile />
+        </div>
+      )}
+
+      {showSecurity && (
+        <div>
+          <Security />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Settingsindex;
---
+++ src/Pages/Settings/components/Security.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Security = () => {
+  return (
+    <div className='text-7xl text-white'>Security</div>
+  )
+}
+
+export default Security
